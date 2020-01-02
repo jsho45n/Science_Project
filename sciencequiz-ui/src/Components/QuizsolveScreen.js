4ddb67fb40e880a6af44ad18f2de26be6fe1bd49
전세훈(Jeon Se hun) <49056122+jsho45n@users.noreply.github.com>
2020-01-02 14:28:43 +0900

    Update QuizsolveScreen.js

--- sciencequiz-ui/src/Components/QuizsolveScreen.js
+++ sciencequiz-ui/src/Components/QuizsolveScreen.js
@@ -37,7 +37,7 @@ const QuizsolveScreen = () => {
 
     return (
         <div>
-            {login ? <Button className="buttons" onClick={removetoken} type="primary"><Link to="/login">로그아웃</Link></Button> : <Button className="buttons" type="primary"><Link to="/login">로그인</Link></Button>}
+            {login ? <Button className="buttons" onClick={removetoken} type="primary"><Link to="/login">로그아웃</Link></Button> : <Button className="buttons" type="primary"><Link to="/login">재로그인</Link></Button>}
             {login ? <p id="font">도전할 단계를 선택하세요</p> : null}
             {login ? <ul><div id="stage"><Link to="/level1"><p id="stage-font">Level 1</p></Link></div><div id="stage"><Link to="level2"><p id="stage-font">Level 2</p></Link></div><div id="stage"><Link to="level3"><p id="stage-font">Level 3</p></Link></div></ul> : null}
         </div>
