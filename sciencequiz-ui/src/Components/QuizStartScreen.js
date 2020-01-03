ec45f306625d541eb9faaf6bfff16f370a67b9d5
전세훈(Jeon Se hun) <49056122+jsho45n@users.noreply.github.com>
2020-01-03 10:41:44 +0900

    Update QuizStartScreen.js

--- sciencequiz-ui/src/Components/QuizStartScreen.js
+++ sciencequiz-ui/src/Components/QuizStartScreen.js
@@ -4,10 +4,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const QuizStartScreen = () => {
+    
+    const showdirector = () => {
+        alert("만든 이 : 전세훈, 유명철, 양승빈");
+    }
+    
     return (
         <div>
             <p>Funny Science Quiz (FSQ)</p>
-            <Button className="button" type="primary">
+            <Button className="button" type="primary" onClick={showdirector}>
                 <Link to="/login">과학퀴즈시작하기</Link>
             </Button>
         </div>
